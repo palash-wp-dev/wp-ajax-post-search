29585d934358788c41590000d3d3e0d66a584206
palash-wp-dev <raps.palash.840@gmail.com>
2025-03-08 13:22:46 +0600

    Added some code in the main file

--- wp-ajax-post-search.php
+++ wp-ajax-post-search.php
@@ -5,3 +5,11 @@
  * Version: 1.0
  * Author: Shahadat Hossain
  */
+
+ if ( ! defined( 'ABSPATH' ) ) exit;
+
+ function ajax_enqueue_scripts() {
+    wp_enqueue_script( 'ajax-script', plugin_dir_url( __FILE__ ) . 'search.js', [ 'jquery' ], null, true );
+ }
+
+ add_action( 'wp_enqueue_scripts', 'ajax_enqueue_scripts' );
