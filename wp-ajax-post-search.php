7f057f2be50d4059fa76f4f4abefdd37cbd9a77f
palash-wp-dev <raps.palash.840@gmail.com>
2025-03-09 16:44:31 +0600

    Added some code in the main file

--- wp-ajax-post-search.php
+++ wp-ajax-post-search.php
@@ -8,8 +8,31 @@
 
  if ( ! defined( 'ABSPATH' ) ) exit;
 
+ // script enqusion
  function ajax_enqueue_scripts() {
     wp_enqueue_script( 'ajax-script', plugin_dir_url( __FILE__ ) . 'search.js', [ 'jquery' ], null, true );
+
+    wp_localize_script( 'ajax-script', 'ajax_object', [ 'ajax_url' => admin_url( 'admin-ajax.php' ) ] );
+ }
+ add_action( 'wp_enqueue_scripts', 'ajax_enqueue_scripts' );
+
+ // ajax code
+ function search_post() {
+   $search_query = isset( $_POST['query'] ) ? sanitize_text_field( $_POST['query'] ) : '';
+
+   if ( empty( $search_query ) ) {
+      wp_send_json_error( 'No search query provided.' );
+      wp_die();
+   }
+
+   $args = [
+      'post_type'      => 'post',
+      'posts_per_page' => 5,
+      's'              => $search_query
+   ];
+
+   $query = new WP_Query($args);
  }
 
- add_action( 'wp_enqueue_scripts', 'ajax_enqueue_scripts' );
+ add_action( 'wp_ajax_search_post', 'search_post' );
+ add_action( 'wp_ajax_nopriv_search_post', 'search_post' );
